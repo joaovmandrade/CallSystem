a9c5131bc24a04c02f2932b5ccd1fe0fd6ceed38
João Vitor <joaovitor2004andrade@exemplo.com>
2025-11-22 19:39:23 -0300

    Login Page

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './signin.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
+export default function SignIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return(
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo Call System" />
+        </div>
+
+        <form>
+          <h1>Entrar</h1>
+          <input 
+            type="text" 
+            placeholder="Usuário" 
+            value={email} 
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+          <input 
+            type="password" 
+            placeholder="Senha" 
+            value={password} 
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+          <input type="submit" value="Acessar" />
+        </form>
+
+        <Link to="/register">Não possui uma conta? Cadastre-se</Link>
+
+      </div>
+    </div>
+  );
+}
