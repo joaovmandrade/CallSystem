90a68bf826924be1ad809b29f8f0a19f16f6bddf
João Vitor <joaovitor2004andrade@exemplo.com>
2025-11-25 00:07:35 -0300

    Firebase Config

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7Y_4U-NxlkZkiCPW-jT-3pNV3X7yvrg0",
+  authDomain: "callsystem-b726d.firebaseapp.com",
+  projectId: "callsystem-b726d",
+  storageBucket: "callsystem-b726d.firebasestorage.app",
+  messagingSenderId: "930283097776",
+  appId: "1:930283097776:web:8a06e0ffd38c75d1323575",
+}
